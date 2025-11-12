1c928961eaac680fe42846d34aca666063d3ace8
eliasBaron <eliasnbaron@gmail.com>
2025-11-12 15:08:23 -0300

    refactor: update video recording configuration and comment out unused browser project

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Screenshot on failure */
     screenshot: "only-on-failure",
 
-    /* Save video even on success */
+    /* Video recording */
     video: "on",
   },
 
@@ -45,10 +45,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
     {
       name: "webkit",
